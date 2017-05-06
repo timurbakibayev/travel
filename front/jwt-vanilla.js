14092604ae897f66ea316897afe2b4f440597083
Timur_hnimdvi <timurbakibayev@gmail.com>
2017-05-07 00:04:39 +0600

    Editing trips

--- front/jwt-vanilla.js
+++ front/jwt-vanilla.js
@@ -1,3 +1,5 @@
+var editTripId;
+
 function getToken() {
   var loginUrl = "http://localhost:8000/auth/"
   var xhr = new XMLHttpRequest();
@@ -109,21 +111,125 @@ function getTrips() {
         var tableHeader = '<table class="table">';
         tableHeader += "<thead><tr><th>Destination</th><th>Start Date</th><th>End Date</th>";
         tableHeader += "<th>Comment</th>";
+        tableHeader += "<th>Days Left</th>";
         tableHeader += "<th>Edit</th></tr></thead>";
 
         var r = new Array(), j = -1;
         for (var key = 0, size = data.length; key < size; key++) {
             console.log(data[key]["destination"])
             r[++j] = '<tr>';
-            r[++j] = '<td>' + data[key]["destination"] + "</td>";
+            r[++j] = '<td>' + data[key]["destination"].replace(/<(?:.|\n)*?>/gm, '') + "</td>";
             r[++j] = '<td>' + data[key]["start_date"] + "</td>";
             r[++j] = '<td>' + data[key]["end_date"] + "</td>";
-            r[++j] = '<td>' + (data[key]["comment"] == null ? "" : data[key]["comment"]) + "</td>";
-            r[++j] = '<td><a href = "#">Edit✎</a>  <a href="#">Delete✘</a> </td>';
+            r[++j] = '<td>' + (data[key]["comment"] == null ? "" : data[key]["comment"].replace(/<(?:.|\n)*?>/gm, '')) + "</td>";
+            r[++j] = '<td>' + (data[key]["days_left"] == 0? "" : data[key]["days_left"]) + "</td>";
+            r[++j] = '<td><a href="#" onclick="editTrip(' + data[key]["id"] + ')">Edit 🖉</a> '+
+                ' <a href="#" onclick="deleteTrip(' + data[key]["id"] + ", '" + data[key]["destination"]+ "'" + ')">Delete ✘</a> </td>';
             r[++j] = '</tr>';
         }
         resultElement.innerHTML = tableHeader + r.join('') + "</table>";
     }
   });
   xhr.send(null);
+}
+
+function newTrip() {
+  var token=localStorage.getItem('token');
+  var url = "http://localhost:8000/trips/";
+  var xhr = new XMLHttpRequest();
+  var destinationElement = document.getElementById('new_destination');
+  var startDateElement = document.getElementById('new_start_date');
+  var endDateElement = document.getElementById('new_end_date');
+  var commentElement = document.getElementById('new_comment');
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader("Authorization", "JWT " + token);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  xhr.addEventListener('load', function() {
+    var data = JSON.parse(this.response);
+    console.log(data);
+    if (this.status == 201) {
+        getTrips();
+        document.getElementById('form_new_trip').reset();
+    } else {
+        alert("There's some problem with your input.")
+    }
+  });
+  var sendObject = JSON.stringify({destination: destinationElement.value,
+                                    start_date: startDateElement.value,
+                                    end_date: endDateElement.value,
+                                    comment: commentElement.value});
+  console.log("Sending",sendObject);
+  xhr.send(sendObject);
+}
+
+function deleteTrip(id, destination){
+  if (confirm("Delete " + destination + "?")) {
+      var token = localStorage.getItem('token');
+      var url = "http://localhost:8000/trips/" + id.toString() + "/"
+      var xhr = new XMLHttpRequest();
+      var destinationElement = document.getElementById('new_destination');
+      var startDateElement = document.getElementById('new_start_date');
+      var endDateElement = document.getElementById('new_end_date');
+      xhr.open('DELETE', url, true);
+      xhr.setRequestHeader("Authorization", "JWT " + token);
+      xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+      xhr.addEventListener('load', function () {
+          getTrips();
+      });
+      xhr.send(null);
+  }
+}
+
+function editTrip(id){
+  var token=localStorage.getItem('token');
+  var url = "http://localhost:8000/trips/"+id.toString()+"/";
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.setRequestHeader("Authorization", "JWT " + token);
+  xhr.addEventListener('load', function() {
+    var data = JSON.parse(this.response);
+    console.log(data);
+    console.log(this.status);
+    openEditForm(data);
+  });
+  xhr.send(null);
+}
+
+function openEditForm(data) {
+    var token=localStorage.getItem('token');
+    var url = "http://localhost:8000/trips/"
+    var xhr = new XMLHttpRequest();
+    var modalHeader = document.getElementById('edit_modal_header');
+    modalHeader.innerHTML = "Edit " + data["destination"];
+    window.editTripId = data["id"];
+    $('#edit_destination').val(data["destination"]);
+    $('#edit_start_date').val(data["start_date"]);
+    $('#edit_end_date').val(data["end_date"]);
+    $('#edit_comment').val(data["comment"]);
+    $('#editModalForm').modal('show');
+}
+
+function saveChanges() {
+    var tripId = window.editTripId.toString();
+    $('#editModalForm').modal('hide');
+    var editDestionation = $('#edit_destination').val();
+    var editComment = $('#edit_comment').val();
+    var editStartDate = $('#edit_start_date').val();
+    var editEndDate = $('#edit_end_date').val();
+
+      var token=localStorage.getItem('token');
+      var url = "http://localhost:8000/trips/"+tripId+"/";
+      var xhr = new XMLHttpRequest();
+      xhr.open('PUT', url, true);
+      xhr.setRequestHeader("Authorization", "JWT " + token);
+      xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+      xhr.addEventListener('load', function() {
+          getTrips();
+      });
+      var sendObject = JSON.stringify({destination: editDestionation,
+                                        start_date: editStartDate,
+                                        end_date: editEndDate,
+                                        comment: editComment});
+      console.log("Sending",sendObject);
+      xhr.send(sendObject);
 }
